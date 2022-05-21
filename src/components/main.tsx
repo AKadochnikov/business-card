2a1b598b312b01a5f15cbe632dc2da030af781ae
Alexander <alex.kadoh@mail.ru>
2022-05-21 15:37:24 +0700

    create main page

---
+++ src/components/main.tsx
@@ -0,0 +1,36 @@
+import {useState} from "react";
+
+function Main ():JSX.Element {
+    const [isRightPanelActive, setIsRightPanelActive] = useState(true);
+    document.title = "Владимир Кадочников"
+    const handleChangePanel = () => {
+        setIsRightPanelActive(!isRightPanelActive)
+    }
+    return (
+        <>
+            <div className={`container ${isRightPanelActive? 'right-panel-active': ''}`} id="container">
+                <div className="form-container sign-up-container">
+                    <form action="#">
+                        <h1>DevOps engineer info</h1>
+                    </form>
+                </div>
+                <div className="form-container sign-in-container">
+                    <form action="#">
+                        <h1>Photograph info</h1>
+                    </form>
+                </div>
+                <div className="overlay-container">
+                    <div className="overlay">
+                        <div onClick={handleChangePanel} className="overlay-panel overlay-left">
+                            <h1>About photograph</h1>
+                        </div>
+                        <div onClick={handleChangePanel} className="overlay-panel overlay-right">
+                            <h1>About devOps engineer</h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>)
+}
+
+export default Main;
